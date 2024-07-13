328475676588666135b1d324da78eb1b80a58013
ksvfs <tsykunowww@gmail.com>
2024-07-13 20:35:48 +0300

    Add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import Page1 from './pages/Page1';
+import Page2 from './pages/Page2';
+import Page3 from './pages/Page3';
+import NotFoundPage from './pages/NotFoundPage';
+
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+        <Route path="/page3" element={<Page3 />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
 }
