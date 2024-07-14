8bc5059ced9f4186a06f57ec76cc6af9a109dc97
ksvfs <tsykunowww@gmail.com>
2024-07-14 10:38:26 +0300

    Add header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,35 @@
 import { NavLink } from 'react-router-dom';
 
+import styles from './Header.module.scss';
+
+type NavLinkRenderProps = {
+  isActive: boolean;
+  isPending: boolean;
+  isTransitioning: boolean;
+};
+
+function getNavLinkClassName({ isActive }: NavLinkRenderProps) {
+  return isActive ? styles.navLinkActive : styles.navLink;
+}
+
 export default function Header() {
   return (
-    <header>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/page1">Page 1</NavLink>
-      <NavLink to="/page2">Page 2</NavLink>
-      <NavLink to="/page3">Page 3</NavLink>
+    <header className={styles.header}>
+      <NavLink to="/" className={getNavLinkClassName}>
+        Home
+      </NavLink>
+
+      <NavLink to="/page1" className={getNavLinkClassName}>
+        Page 1
+      </NavLink>
+
+      <NavLink to="/page2" className={getNavLinkClassName}>
+        Page 2
+      </NavLink>
+
+      <NavLink to="/page3" className={getNavLinkClassName}>
+        Page 3
+      </NavLink>
     </header>
   );
 }
