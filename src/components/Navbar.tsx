a61b5cccea04188e94d2493f99d65dac269201a3
ksvfs <tsykunowww@gmail.com>
2024-07-15 19:14:38 +0300

    Rename Navbar styles file

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { NavLink } from 'react-router-dom';
 
-import styles from './Header.module.scss';
+import styles from './Navbar.module.scss';
 
 type NavLinkRenderProps = {
   isActive: boolean;
