2dccaa11f09915f53e83a530818457abcd919226
ksvfs <tsykunowww@gmail.com>
2024-07-15 20:12:50 +0300

    Add loading spinner

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -11,4 +11,27 @@ export default {
       <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
     </svg>
   ),
+  // https://feathericons.com/?query=loader
+  loading: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <line x1="12" y1="2" x2="12" y2="6"></line>
+      <line x1="12" y1="18" x2="12" y2="22"></line>
+      <line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line>
+      <line x1="16.24" y1="16.24" x2="19.07" y2="19.07"></line>
+      <line x1="2" y1="12" x2="6" y2="12"></line>
+      <line x1="18" y1="12" x2="22" y2="12"></line>
+      <line x1="4.93" y1="19.07" x2="7.76" y2="16.24"></line>
+      <line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line>
+    </svg>
+  ),
 };
--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,6 @@
+import styles from './Loading.module.scss';
+import icons from '../assets/icons.tsx';
+
 export default function Loading() {
-  return (
-    <section>
-      <p>Loading...</p>
-    </section>
-  );
+  return <div className={styles.loading}>{icons.loading}</div>;
 }
