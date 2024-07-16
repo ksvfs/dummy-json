17f4132de07a28a1a6de1589615787eb56400eef
ksvfs <tsykunowww@gmail.com>
2024-07-16 13:06:32 +0300

    Add dark mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,9 @@
+import { useState, useLayoutEffect } from 'react';
+
 import { NavLink } from 'react-router-dom';
 
 import styles from './Navbar.module.scss';
+import icons from '../assets/icons.tsx';
 
 type NavLinkRenderProps = {
   isActive: boolean;
@@ -13,11 +16,47 @@ function getNavLinkClassName({ isActive }: NavLinkRenderProps) {
 }
 
 export default function Navbar() {
+  const [themeIcon, setThemeIcon] = useState<JSX.Element>();
+
+  useLayoutEffect(setTheme, []);
+
+  function setTheme() {
+    const localTheme = localStorage.getItem('theme');
+
+    if (localTheme === 'dark') {
+      document.body.classList.add('dark');
+      setThemeIcon(icons.sun);
+    } else {
+      document.body.classList.add('light');
+      setThemeIcon(icons.moon);
+    }
+  }
+
+  function switchTheme() {
+    if (document.body.classList.contains('light')) {
+      document.body.classList.remove('light');
+      document.body.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+      setThemeIcon(icons.sun);
+    } else if (document.body.classList.contains('dark')) {
+      document.body.classList.remove('dark');
+      document.body.classList.add('light');
+      localStorage.setItem('theme', 'light');
+      setThemeIcon(icons.moon);
+    }
+  }
+
   return (
     <header className={styles.header}>
-      <NavLink to="/products" className={getNavLinkClassName}>
-        Товары
-      </NavLink>
+      <nav>
+        <NavLink to="/products" className={getNavLinkClassName}>
+          Товары
+        </NavLink>
+      </nav>
+
+      <button className={styles.colorThemeSwitch} onClick={switchTheme}>
+        {themeIcon}
+      </button>
     </header>
   );
 }
