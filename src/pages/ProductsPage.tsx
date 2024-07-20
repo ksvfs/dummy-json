23365aa1bcd241d5f832cc4e1e6e38185588ca2c
ksvfs <tsykunowww@gmail.com>
2024-07-20 13:50:40 +0300

    Refactor data fetching

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Products from '../components/Products';
 import Loading from '../components/Loading';
@@ -12,24 +12,34 @@ const uglyProductsIds = new Set([
   132, 133, 134, 135, 136, 167, 168, 169, 170, 171,
 ]);
 
+/*
+  У некоторых фотографий в DummyJSON API есть чёрные полосы,
+  которые ужасно выглядят в светлой теме.
+  Я решил их просто отфильтровать.
+  В реальных проектах обещаю не убирать с сайта товары
+  просто потому, что мне не нравятся их фотографии.
+*/
+
 export default function ProductsPage() {
   const [products, setProducts] = useState<Product[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(0);
   const [error, setError] = useState(false);
+  const [currentPage, setCurrentPage] = useState(0);
   const [canFetchMore, setCanFetchMore] = useState(true);
 
   useEffect(() => {
-    async function fetchData() {
+    async function fetchData(abortController: AbortController) {
       if (!canFetchMore) return;
 
       try {
         setIsLoading(true);
 
+        const select = 'id,thumbnail,price,rating,title';
+        const limit = 30;
+        const skip = currentPage * limit;
+
         const response = await fetch(
-          `https://dummyjson.com/products?select=id,thumbnail,price,rating,title&limit=30&skip=${
-            currentPage * 30
-          }`,
+          `https://dummyjson.com/products?select=${select}&limit=${limit}&skip=${skip}`,
           { signal: abortController.signal }
         );
 
@@ -38,33 +48,36 @@ export default function ProductsPage() {
         }
 
         const data = await response.json();
-        const products = filterProducts(data.products);
+        const newProducts = data.products.filter(
+          (product: Product) => !uglyProductsIds.has(product.id)
+        );
 
-        setProducts((prevProducts) => [...prevProducts, ...products]);
-        setCanFetchMore((currentPage + 1) * 30 < data.total);
-        setCurrentPage((prevCurrentPage) => prevCurrentPage + 1);
+        setProducts([...products, ...newProducts]);
+        setCanFetchMore((currentPage + 1) * limit < data.total);
+        setCurrentPage(currentPage + 1);
         setIsLoading(false);
-      } catch (err) {
-        if (!abortController.signal.aborted) {
-          setError(true);
-          setIsLoading(false);
-        }
+      } catch {
+        if (abortController.signal.aborted) return;
+        setError(true);
+        setIsLoading(false);
       }
     }
 
     const abortController = new AbortController();
 
     if (currentPage === 0) {
-      fetchData();
+      fetchData(abortController);
     }
 
     let fetchCalled = false;
 
     function handlePageEnd() {
+      if (fetchCalled) return;
+
       const pixelsLeft = document.body.scrollHeight - (window.scrollY + window.innerHeight);
 
-      if (pixelsLeft < 500 && !fetchCalled) {
-        fetchData();
+      if (pixelsLeft < 500) {
+        fetchData(abortController);
         fetchCalled = true;
       }
     }
@@ -75,11 +88,7 @@ export default function ProductsPage() {
       document.removeEventListener('scroll', handlePageEnd);
       abortController.abort();
     };
-  }, [canFetchMore, currentPage]);
-
-  function filterProducts(data: Product[]): Product[] {
-    return data.filter((product) => !uglyProductsIds.has(product.id));
-  }
+  }, [products, currentPage, canFetchMore]);
 
   if (isLoading && currentPage === 0) {
     return <Loading />;
