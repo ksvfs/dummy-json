5f8bab5307c4a52b0ff91b8c407534e815037e85
ksvfs <tsykunowww@gmail.com>
2024-07-21 18:08:51 +0300

    Add search form

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,17 +1,47 @@
-import Products from '../components/Products';
 import Loading from '../components/Loading';
 import FetchingError from '../components/FetchingError';
 
 import useProducts from '../hooks/useProducts.ts';
+
+import styles from './ProductsPage.module.scss';
+import icons from '../assets/icons.tsx';
 
 export default function ProductsPage() {
   const { products, currentPage, isLoading, error } = useProducts();
 
   if (isLoading && currentPage === 0) {
     return <Loading />;
-  } else if (error) {
-    return <FetchingError />;
-  } else {
-    return <Products products={products} />;
   }
+
+  if (error) {
+    return <FetchingError />;
+  }
+
+  return (
+    <>
+      <form className={styles.searchForm}>
+        <input className={styles.searchInput} type="text" placeholder="Поиск товара" />
+        <button className={styles.searchButton}>{icons.search}</button>
+      </form>
+
+      <section className={styles.section}>
+        {products.map((product) => (
+          <div className={styles.product} key={product.id}>
+            <div className={styles.thumbnail}>
+              <img src={product.thumbnail} alt={product.title} />
+            </div>
+
+            <div className={styles.priceAndRating}>
+              <div className={styles.price}>${product.price}</div>
+              <div className={styles.rating}>
+                {icons.star} {product.rating}
+              </div>
+            </div>
+
+            <div className={styles.title}>{product.title}</div>
+          </div>
+        ))}
+      </section>
+    </>
+  );
 }
