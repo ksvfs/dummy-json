8fd57a69f43e96e0612517c76f93851e250076d2
ksvfs <tsykunowww@gmail.com>
2024-07-28 13:03:57 +0300

    Add navbar colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.tsx'],
+  darkMode: 'selector',
   theme: {
     colors: {
       'light': 'rgb(255, 255, 255)',
       'dark': 'rgb(10, 10, 10)',
       'border-light': 'rgb(200, 200, 200)',
       'border-dark': 'rgb(100, 100, 100)',
+      'navbar-light': 'rgba(255, 255, 255, 0.7)',
+      'navbar-dark': 'rgba(10, 10, 10, 0.7)',
     },
     extend: {},
   },
