29bbf976f534011ef0367794b9d9f48a5ddda217
ksvfs <tsykunowww@gmail.com>
2024-07-28 09:56:48 +0300

    Add custom font

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
+import './index.css';
+
 const isLocalhost = window.location.hostname === 'localhost';
 const basename = isLocalhost ? '' : '/dummy-json/';
 
